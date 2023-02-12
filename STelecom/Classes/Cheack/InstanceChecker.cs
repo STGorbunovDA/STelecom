4909056e35a560f76a8af9bc00915ad497943114
STGorbunovDA <bous07@mail.ru>
2023-02-12 18:52:09 +0800

    Add class tracking whether the form is open

---
+++ STelecom/Classes/Cheack/InstanceChecker.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading;
+
+namespace STelecom.Classes.Cheack
+{
+    class InstanceChecker
+    {
+        static readonly Mutex mutex = new Mutex(false, "STelecom");
+        public static bool TakeMemory()
+        {
+            return mutex.WaitOne();
+        }
+    }
+}
