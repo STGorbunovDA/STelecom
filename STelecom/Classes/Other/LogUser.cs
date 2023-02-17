724afde68e330b06252cbd4a44321448a44d9ad1
STGorbunovDA <bous07@mail.ru>
2023-02-18 01:19:43 +0800

    #27

---
+++ STelecom/Classes/Other/LogUser.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace STelecom.Classes.Other
+{
+    internal class LogUser
+    {
+        internal static void LogExceptionUserSaveFilePC(string exception)
+        {
+            try
+            {
+                DateTime today = DateTime.Now;
+
+                string fileNamePath = $@"C:\Documents_ServiceTelekom\Log\Ошибка-{today.ToString("dd.MM.yyyy")}.txt";
+
+                if (!File.Exists($@"C:\Documents_ServiceTelekom\Log\"))
+                    Directory.CreateDirectory($@"C:\Documents_ServiceTelekom\Log\");
+
+                using (StreamWriter sw = new StreamWriter(fileNamePath, true, Encoding.Unicode))
+                {
+                    sw.WriteLine($"{exception}\t{today}");
+                    sw.WriteLine($"__________________________");
+                    sw.WriteLine(string.Empty);
+                }
+
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Файл не сохранен!(LogExceptionUserSaveFilePC)");
+            }
+        }
+    }
+}
